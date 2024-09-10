5c6400fbb044d301e1fdcebd5dd9a26bcf13b413
Nadeem khan <khannadeem1892@gmail.com>
2024-09-10 15:49:55 +0530

    cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+var cors = require('cors')
 require("./dataBase");
 const router = require("./api/route");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use("/", router);
 app.use(cors())
+app.use("/", router);
+
 
 module.exports = app;
